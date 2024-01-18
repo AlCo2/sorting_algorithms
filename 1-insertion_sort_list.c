fcc65d0192163dfc94c7a394f9dd235f3d4bcf69
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-01-18 15:00:48 +0100

    finished second task 'insetionSort'

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - insertion sort on doublyLinkedList
+ *
+ * @list: doubly linked list
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i = *list;
+	listint_t *j;
+	listint_t *key;
+
+	i = i->next;
+	while (i)
+	{
+		key = i;
+		j = i->prev;
+		while (j && j->n > key->n)
+		{
+			listint_t *tempP = j->prev;
+			listint_t *tempN = key->next;
+
+			if (tempP != NULL)
+			{
+				tempP->next = key;
+			}
+			else
+			{
+				*list = key;
+			}
+			key->prev = tempP;
+			key->next = j;
+			j->prev = key;
+			j->next = tempN;
+			if (tempN != NULL)
+			{
+				tempN->prev = j;
+			}
+			print_list(*list);
+			j = key->prev;
+		}
+		i = i->next;
+	}
+}
+
